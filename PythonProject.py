96b06cbe96df1871900db100fbcc646e02354c1f
Delaney Ramalho <delaneyftf@gmail.com>
2018-04-04 12:36:58 -0400

    Merged Delaney and Olufemi's code

--- PythonProject.py
+++ PythonProject.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy import stats, integrate
+import statsmodels.api as sm
+
 
 def VisualizeCount(data):
   print("Count: " + str(data.count()))
@@ -77,6 +80,29 @@ def AnalyzeSingleRegression(data):
   ax.scatter(x, y)
 
   plt.show()
+
+def AnalyzeMultipleRegression():
+  xlpath = "crime.xls"
+
+  data = pd.read_excel(xlpath, header=0, error_bad_lines=False)
+  #print("\n{}\n".format(data))
+
+  targets = pd.DataFrame(data, columns=["X1"])
+
+  X = data[["X2","X3","X4","X5","X6","X7"]]
+  y = targets["X1"]
+  X = sm.add_constant(X)
+
+  model = sm.OLS(y, X).fit()
+  predictions = model.predict(X)
+
+  plt.scatter(predictions, y, s=30, c='r', marker='+', zorder=10)
+  fit = np.polyfit(predictions, y, deg=1)
+  plt.plot(predictions, fit[0] * predictions + fit[1], color='orange')
+  plt.xlabel("Predicted Values from model")
+  plt.ylabel("Actual Values Prices")
+  plt.show()
+
   
 def main():
 
@@ -126,6 +152,8 @@ def main():
     AnalyzeSingleRegression(X5)
     AnalyzeSingleRegression(X6)
     AnalyzeSingleRegression(X7)
-   
+
+    AnalyzeMultipleRegression()
+  
 if __name__ == "__main__":
   main()
